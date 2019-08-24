3e773606ad98ffe380013b2431437dd54eca2f24
Eric Laursen <ericjlaursen@gmail.com>
2019-08-24 14:15:04 -0400

    beginner react code

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css";
+
+const App = () => {
+	return(<div>
+		<p>App Here!</p>
+	</div>)
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
