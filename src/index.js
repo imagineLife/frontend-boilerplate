79c5432270ae30b320ca66ad4b8bff86f40193ef
Eric Laursen <ericjlaursen@gmail.com>
2019-08-24 14:37:44 -0400

    includes && tries rendering img

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import pngImage from './static/say-what.png'
 import "./index.css";
 
 const App = () => {
 	return(<div>
+	{pngImage}
 		<p>App Here!</p>
+	}
 	</div>)
 }
 
